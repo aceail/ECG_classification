a955292b552466f777d36f6e5cab7147b100a74a
aceail <38589098+aceail@users.noreply.github.com>
2022-05-24 15:22:39 +0900

    Create ResNet_50.py

---
+++ ResNet_50.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 01:09:17 2016
+@author: stephen
+"""
+
+from tensorflow import keras
+import numpy as np
+import pandas as pd
+import tensorflow.keras.backend as K
+
+
+np.random.seed(813306)
+ 
+def build_resnet(input_shape, n_feature_maps, nb_classes):
+    print ('build conv_x')
+    x = keras.layers.Input(shape=(input_shape))
+    conv_x = keras.layers.BatchNormalization()(x)
+    conv_x = keras.layers.Conv2D(n_feature_maps, 8, 1, padding='same')(conv_x)
+    conv_x = keras.layers.BatchNormalization()(conv_x)
+    conv_x = keras.layers.Activation('relu')(conv_x)
+     
+    print ('build conv_y')
+    conv_y = keras.layers.Conv2D(n_feature_maps, 5, 1, padding='same')(conv_x)
+    conv_y = keras.layers.BatchNormalization()(conv_y)
+    conv_y = keras.layers.Activation('relu')(conv_y)
+     
+    print ('build conv_z')
+    conv_z = keras.layers.Conv2D(n_feature_maps, 3, 1, padding='same')(conv_y)
+    conv_z = keras.layers.BatchNormalization()(conv_z)
+     
+    is_expand_channels = not (input_shape[-1] == n_feature_maps)
+    if is_expand_channels:
+        shortcut_y = keras.layers.Conv2D(n_feature_maps, 1, 1,padding='same')(x)
+        shortcut_y = keras.layers.BatchNormalization()(shortcut_y)
+    else:
+        shortcut_y = keras.layers.BatchNormalization()(x)
+    print ('Merging skip connection')
+    y = keras.layers.Add()([shortcut_y, conv_z])
+    y = keras.layers.Activation('relu')(y)
+     
+    print ('build conv_x')
+    x1 = y
+    conv_x = keras.layers.Conv2D(n_feature_maps*2, 8, 1, padding='same')(x1)
+    conv_x = keras.layers.BatchNormalization()(conv_x)
+    conv_x = keras.layers.Activation('relu')(conv_x)
+     
+    print ('build conv_y')
+    conv_y = keras.layers.Conv2D(n_feature_maps*2, 5, 1, padding='same')(conv_x)
+    conv_y = keras.layers.BatchNormalization()(conv_y)
+    conv_y = keras.layers.Activation('relu')(conv_y)
+     
+    print ('build conv_z')
+    conv_z = keras.layers.Conv2D(n_feature_maps*2, 3, 1, padding='same')(conv_y)
+    conv_z = keras.layers.BatchNormalization()(conv_z)
+     
+    is_expand_channels = not (input_shape[-1] == n_feature_maps*2)
+    if is_expand_channels:
+        shortcut_y = keras.layers.Conv2D(n_feature_maps*2, 1, 1,padding='same')(x1)
+        shortcut_y = keras.layers.BatchNormalization()(shortcut_y)
+    else:
+        shortcut_y = keras.layers.BatchNormalization()(x1)
+    print ('Merging skip connection')
+    y = keras.layers.Add()([shortcut_y, conv_z])
+    y = keras.layers.Activation('relu')(y)
+     
+    print ('build conv_x')
+    x1 = y
+    conv_x = keras.layers.Conv2D(n_feature_maps*2, 8, 1, padding='same')(x1)
+    conv_x = keras.layers.BatchNormalization()(conv_x)
+    conv_x = keras.layers.Activation('relu')(conv_x)
+     
+    print ('build conv_y')
+    conv_y = keras.layers.Conv2D(n_feature_maps*2, 5, 1, padding='same')(conv_x)
+    conv_y = keras.layers.BatchNormalization()(conv_y)
+    conv_y = keras.layers.Activation('relu')(conv_y)
+     
+    print ('build conv_z')
+    conv_z = keras.layers.Conv2D(n_feature_maps*2, 3, 1, padding='same')(conv_y)
+    conv_z = keras.layers.BatchNormalization()(conv_z)
+
+    is_expand_channels = not (input_shape[-1] == n_feature_maps*2)
+    if is_expand_channels:
+        shortcut_y = keras.layers.Conv2D(n_feature_maps*2, 1, 1,padding='same')(x1)
+        shortcut_y = keras.layers.BatchNormalization()(shortcut_y)
+    else:
+        shortcut_y = keras.layers.BatchNormalization()(x1)
+    print ('Merging skip connection')
+    y = keras.layers.Add()([shortcut_y, conv_z])
+    y = keras.layers.Activation('relu')(y)
+     
+    full = keras.layers.GlobalAveragePooling2D()(y)
+    out = keras.layers.Dense(nb_classes, activation='softmax')(full)
+    print ('        -- model was built.')
+    return x, out
+ 
+ nb_classes = len(np.unique(y_test))
+batch_size = min(X_train.shape[0]/10, 16)
+
+X_train = X_train.reshape(X_train.shape + (1,1,))
+X_test = X_test.reshape(X_test.shape + (1,1,))
+
+def get_f1(y_true, y_pred): #taken from old keras source code
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    recall = true_positives / (possible_positives + K.epsilon())
+    f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
+    return f1_val
+    
+x , y = build_resnet(X_train.shape[1:], 64, 5)
+model = keras.models.Model(inputs=x, outputs=y)
+
+optimizer = keras.optimizers.Adam()
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizer,
+              metrics=['categorical_accuracy',get_f1])
+
+reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5,
+                  patience=50, min_lr=0.0001) 
+
+hist = model.fit(X_train, y_train, batch_size=128, epochs=100,
+          verbose=1, validation_data=(X_test, y_test), callbacks = [reduce_lr])
+# log = pd.DataFrame(hist.history)
+# print(log.loc[log['loss'].idxmin]['loss'], log.loc[log['loss'].idxmin]['val_acc'])
+
+pred= model.predict(X_test)
+
+print(classification_report([i.argmax() for i in y_test.values], [i.argmax() for i in pred]))
+
+pd.DataFrame([i.argmax() for i in pred]).value_counts()
